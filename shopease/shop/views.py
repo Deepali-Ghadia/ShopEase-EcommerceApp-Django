0de6780735981a0eaa61c34a0fe9453fece61dea
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2024-03-15 00:59:52 +0530

    defined functions to render data on respective urls of shop app

--- shopease/shop/views.py
+++ shopease/shop/views.py
@@ -4,3 +4,26 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'shop/index.html')
+
+def about(request):
+    return HttpResponse("About Page of Shop App")
+
+
+def contact(request):
+    return HttpResponse("Contact Page of Shop App")
+
+
+def tracker(request):
+    return HttpResponse("Tracker Page of Shop App")
+
+
+def search(request):
+    return HttpResponse("Search Page of Shop App")
+
+
+def view_product(request):
+    return HttpResponse("Product Details Page of Shop App")
+
+
+def checkout(request):
+    return HttpResponse("Checkout Page of Shop App")
