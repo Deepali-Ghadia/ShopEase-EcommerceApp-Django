509bad705276e55fe91a5046d8390fd9e877307f
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2024-03-15 00:59:23 +0530

    defined pipeline(urls) for the shop app

--- shopease/shop/urls.py
+++ shopease/shop/urls.py
@@ -5,4 +5,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="shop_app_index"),
+    path("about/", views.index, name="shop_app_about"),
+    path("contact_us/", views.contact, name="shop_app_contact_us"),
+    path("tracker/", views.tracker, name="shop_app_tracker"),
+    path("search/", views.search, name="shop_app_search"),
+    path("view_product/", views.view_product, name="shop_app_view_product"),
+    path("checkout/", views.checkout, name="shop_app_checkout"),
+    
 ]
